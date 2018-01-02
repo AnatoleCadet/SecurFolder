3197d9e4d4f7b08f5c6978d57b961cad4b232f5d
Toto <anatolec@hotmail.com>
2018-01-02 12:19:36 -0500

    Implementation the event of the component btnLoadFolder

--- SecurFolder/SecurFolder/Form1.Designer.cs
+++ SecurFolder/SecurFolder/Form1.Designer.cs
@@ -30,24 +30,27 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.btnLoadFolder = new System.Windows.Forms.Button();
+            this.txtFolderName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.lblItemNumber = new System.Windows.Forms.Label();
+            this.lblFileNumber = new System.Windows.Forms.Label();
             this.lblDateCreated = new System.Windows.Forms.Label();
-            this.lblAuthor = new System.Windows.Forms.Label();
+            this.lblDirectoryNumber = new System.Windows.Forms.Label();
+            this.fbdFolderSelected = new System.Windows.Forms.FolderBrowserDialog();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(101, 13);
+            this.label1.Location = new System.Drawing.Point(161, 13);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(146, 20);
             this.label1.TabIndex = 0;
@@ -56,7 +59,7 @@
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.btnLoadFolder);
-            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.txtFolderName);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Location = new System.Drawing.Point(12, 48);
             this.groupBox1.Name = "groupBox1";
@@ -64,14 +67,22 @@
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             // 
-            // button1
+            // btnLoadFolder
             // 
-            this.button1.Location = new System.Drawing.Point(379, 196);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Quitter";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnLoadFolder.Location = new System.Drawing.Point(368, 15);
+            this.btnLoadFolder.Name = "btnLoadFolder";
+            this.btnLoadFolder.Size = new System.Drawing.Size(49, 22);
+            this.btnLoadFolder.TabIndex = 2;
+            this.btnLoadFolder.Text = "...";
+            this.btnLoadFolder.UseVisualStyleBackColor = true;
+            this.btnLoadFolder.Click += new System.EventHandler(this.btnLoadFolder_Click);
+            // 
+            // txtFolderName
+            // 
+            this.txtFolderName.Location = new System.Drawing.Point(104, 15);
+            this.txtFolderName.Name = "txtFolderName";
+            this.txtFolderName.Size = new System.Drawing.Size(257, 20);
+            this.txtFolderName.TabIndex = 1;
             // 
             // label2
             // 
@@ -83,35 +94,29 @@
             this.label2.TabIndex = 0;
             this.label2.Text = "Select a folder";
             // 
-            // textBox1
+            // btnClose
             // 
-            this.textBox1.Location = new System.Drawing.Point(104, 15);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(257, 20);
-            this.textBox1.TabIndex = 1;
-            // 
-            // btnLoadFolder
-            // 
-            this.btnLoadFolder.Location = new System.Drawing.Point(368, 15);
-            this.btnLoadFolder.Name = "btnLoadFolder";
-            this.btnLoadFolder.Size = new System.Drawing.Size(49, 22);
-            this.btnLoadFolder.TabIndex = 2;
-            this.btnLoadFolder.Text = "...";
-            this.btnLoadFolder.UseVisualStyleBackColor = true;
+            this.btnClose.Location = new System.Drawing.Point(379, 228);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(17, 114);
+            this.label3.Location = new System.Drawing.Point(36, 19);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.Size = new System.Drawing.Size(78, 13);
             this.label3.TabIndex = 3;
-            this.label3.Text = "Item number :";
+            this.label3.Text = "Number of file :";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(13, 138);
+            this.label4.Location = new System.Drawing.Point(39, 71);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(75, 13);
             this.label4.TabIndex = 4;
@@ -120,57 +125,71 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(44, 162);
+            this.label5.Location = new System.Drawing.Point(9, 45);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.Size = new System.Drawing.Size(105, 13);
             this.label5.TabIndex = 5;
-            this.label5.Text = "Author :";
+            this.label5.Text = "Number of directory :";
             // 
-            // lblItemNumber
+            // lblFileNumber
             // 
-            this.lblItemNumber.AutoSize = true;
-            this.lblItemNumber.Location = new System.Drawing.Point(105, 114);
-            this.lblItemNumber.Name = "lblItemNumber";
-            this.lblItemNumber.Size = new System.Drawing.Size(35, 13);
-            this.lblItemNumber.TabIndex = 6;
-            this.lblItemNumber.Text = "label6";
+            this.lblFileNumber.AutoSize = true;
+            this.lblFileNumber.Location = new System.Drawing.Point(120, 19);
+            this.lblFileNumber.Name = "lblFileNumber";
+            this.lblFileNumber.Size = new System.Drawing.Size(35, 13);
+            this.lblFileNumber.TabIndex = 6;
+            this.lblFileNumber.Text = "label6";
             // 
             // lblDateCreated
             // 
             this.lblDateCreated.AutoSize = true;
-            this.lblDateCreated.Location = new System.Drawing.Point(105, 138);
+            this.lblDateCreated.Location = new System.Drawing.Point(120, 71);
             this.lblDateCreated.Name = "lblDateCreated";
             this.lblDateCreated.Size = new System.Drawing.Size(35, 13);
             this.lblDateCreated.TabIndex = 7;
             this.lblDateCreated.Text = "label7";
             // 
-            // lblAuthor
+            // lblDirectoryNumber
             // 
-            this.lblAuthor.AutoSize = true;
-            this.lblAuthor.Location = new System.Drawing.Point(105, 162);
-            this.lblAuthor.Name = "lblAuthor";
-            this.lblAuthor.Size = new System.Drawing.Size(35, 13);
-            this.lblAuthor.TabIndex = 8;
-            this.lblAuthor.Text = "label8";
+            this.lblDirectoryNumber.AutoSize = true;
+            this.lblDirectoryNumber.Location = new System.Drawing.Point(120, 45);
+            this.lblDirectoryNumber.Name = "lblDirectoryNumber";
+            this.lblDirectoryNumber.Size = new System.Drawing.Size(35, 13);
+            this.lblDirectoryNumber.TabIndex = 8;
+            this.lblDirectoryNumber.Text = "label8";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblDirectoryNumber);
+            this.groupBox2.Controls.Add(this.lblDateCreated);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.lblFileNumber);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Location = new System.Drawing.Point(12, 109);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(442, 100);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(468, 231);
-            this.Controls.Add(this.lblAuthor);
-            this.Controls.Add(this.lblDateCreated);
-            this.Controls.Add(this.lblItemNumber);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(468, 263);
+            this.Controls.Add(this.btnClose);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox2);
+            this.MaximizeBox = false;
             this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SecurFolder";
+            this.Load += new System.EventHandler(this.frmMain_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -181,15 +200,17 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button btnLoadFolder;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtFolderName;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnClose;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label lblItemNumber;
+        private System.Windows.Forms.Label lblFileNumber;
         private System.Windows.Forms.Label lblDateCreated;
-        private System.Windows.Forms.Label lblAuthor;
+        private System.Windows.Forms.Label lblDirectoryNumber;
+        private System.Windows.Forms.FolderBrowserDialog fbdFolderSelected;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
 
