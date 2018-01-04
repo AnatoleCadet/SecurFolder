c613f66e2c4f2d0a4a46e5dd53f6b66e1e42c135
tchotcho <anatole.cadet.1@ulaval.ca>
2018-01-04 07:38:21 -0500

    Adding a new form and add components, and implement the close button of the form (frmLisFolderLock)

---
+++ SecurFolder/SecurFolder/frmListFolderLock.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace SecurFolder
+{
+    partial class frmListFolderLock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClose = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblDirectoryNumber = new System.Windows.Forms.Label();
+            this.lblDateCreated = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblFileNumber = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lstViewListFolder = new System.Windows.Forms.ListView();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(597, 241);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblDirectoryNumber);
+            this.groupBox1.Controls.Add(this.lblDateCreated);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.lblFileNumber);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.lstViewListFolder);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(660, 223);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "List Folder Lock";
+            // 
+            // lblDirectoryNumber
+            // 
+            this.lblDirectoryNumber.AutoSize = true;
+            this.lblDirectoryNumber.Location = new System.Drawing.Point(119, 167);
+            this.lblDirectoryNumber.Name = "lblDirectoryNumber";
+            this.lblDirectoryNumber.Size = new System.Drawing.Size(35, 13);
+            this.lblDirectoryNumber.TabIndex = 20;
+            this.lblDirectoryNumber.Text = "label8";
+            // 
+            // lblDateCreated
+            // 
+            this.lblDateCreated.AutoSize = true;
+            this.lblDateCreated.Location = new System.Drawing.Point(119, 193);
+            this.lblDateCreated.Name = "lblDateCreated";
+            this.lblDateCreated.Size = new System.Drawing.Size(35, 13);
+            this.lblDateCreated.TabIndex = 19;
+            this.lblDateCreated.Text = "label7";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Number of file :";
+            // 
+            // lblFileNumber
+            // 
+            this.lblFileNumber.AutoSize = true;
+            this.lblFileNumber.Location = new System.Drawing.Point(119, 141);
+            this.lblFileNumber.Name = "lblFileNumber";
+            this.lblFileNumber.Size = new System.Drawing.Size(35, 13);
+            this.lblFileNumber.TabIndex = 18;
+            this.lblFileNumber.Text = "label6";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(38, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Date created :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 167);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Number of directory :";
+            // 
+            // lstViewListFolder
+            // 
+            this.lstViewListFolder.Location = new System.Drawing.Point(6, 19);
+            this.lstViewListFolder.Name = "lstViewListFolder";
+            this.lstViewListFolder.Size = new System.Drawing.Size(648, 119);
+            this.lstViewListFolder.TabIndex = 0;
+            this.lstViewListFolder.UseCompatibleStateImageBehavior = false;
+            this.lstViewListFolder.View = System.Windows.Forms.View.Details;
+            // 
+            // frmListFolderLock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 275);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmListFolderLock";
+            this.Text = "Secur Folder / List of folder lock";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblDirectoryNumber;
+        private System.Windows.Forms.Label lblDateCreated;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblFileNumber;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListView lstViewListFolder;
+    }
+}
